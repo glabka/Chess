3e9957c2128940957e261a4e5535ab7af2d15dc9
glabka <glabka333@gmail.com>
2019-05-24 10:21:06 +0200

    Running moves from code.

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+
+import java.util.LinkedList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author glabka
@@ -36,16 +38,47 @@ public class Main {
 //            b2.printBoard();
 //        Rules r1 = new Rules();
 //        r1.testing(b2);
+//        // Testing of moves - user input        
+//        
+//        Board b = new Board();
+//        Player p1 = new Player(Color.WHITE);
+//        Player p2 = new Player(Color.BLACK);
+//        Player currentPlayer = p1;
+//        MovesContainer con = new MovesContainer();
+//        int[] positions = new int[4];
+//        while (true) {
+//            b.printBoard();
+//            Input.read4Int(positions);
+//            if (b.move(currentPlayer, con, positions[0], positions[1], positions[2], positions[3])) {
+//                if (currentPlayer == p1) {
+//                    currentPlayer = p2;
+//                } else {
+//                    currentPlayer = p1;
+//                }
+//            } else {
+//                System.out.println("The move is not possible.");
+//            }
+//
+//        }
+        // Testing of moves - input in code
+        // moves
+        LinkedList<Integer[]> moves = new LinkedList<>();
+        Integer[][] positions = {{6, 4, 4, 4},
+        {1, 4, 2, 4}, 
+        {7, 4, 6, 4},
+        {0, 3, 3, 6},
+        {6, 4, 5, 4}};
+
         Board b = new Board();
         Player p1 = new Player(Color.WHITE);
         Player p2 = new Player(Color.BLACK);
         Player currentPlayer = p1;
         MovesContainer con = new MovesContainer();
-        int[] positions = new int[4];
-        while (true) {
+
+        for (int i = 0; i < positions.length; i++) {
             b.printBoard();
-            Input.read4Int(positions);
-            if (b.move(currentPlayer, con, positions[0], positions[1], positions[2], positions[3])) {
+            System.out.println(positions[i][0] + " " + positions[i][1] + " " + positions[i][2] + " " + positions[i][3]);
+            if (b.move(currentPlayer, con, positions[i][0], positions[i][1], positions[i][2], positions[i][3])) {
                 if (currentPlayer == p1) {
                     currentPlayer = p2;
                 } else {
@@ -54,8 +87,8 @@ public class Main {
             } else {
                 System.out.println("The move is not possible.");
             }
-
         }
+        b.printBoard();
     }
 
 }
